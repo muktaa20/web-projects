3d3be74ba32f137c7f93b6873b59602a695899bc
Mukta Suryavanshi <tulsiramsuryavanshi956@gmail.com>
2025-11-05 21:47:28 +0530

    Background changer project Added

--- reactProjects/my_prjct/src/App.jsx
+++ reactProjects/my_prjct/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-
+import BgChanger from './Components/BgChanger'
 function App() {
 
   let [counter, setCounter] = useState(0)
@@ -20,12 +20,15 @@ function reset(){
 }
 
   return (
-    <>
-    <h1>Counter : {counter}</h1>
-    <button onClick={increase}>Increase{counter}</button>
-    <button onClick={decrease}>Decrease{counter}</button>
-    <button onClick={reset}>Reset{counter}</button>
-    </>
+    <div className='bg-gray-500'>
+    <BgChanger />
+    <h1 className='text-white text-2xl text-center'>Counter : {counter}</h1>
+   <div className='flex justify-center text-center py-4'>
+     <button className='bg-gray-600 text-white px-4 py-2 m-4  rounded-lg' onClick={increase}>Increase {counter}</button>
+    <button className='bg-gray-600 text-white px-4 py-2 m-4 rounded-lg' onClick={decrease}>Decrease{counter}</button>
+    <button className='bg-gray-600 text-white px-4 py-2 m-4 rounded-lg' onClick={reset}>Reset{counter}</button>
+   </div>
+    </div>
   )
 }
 
